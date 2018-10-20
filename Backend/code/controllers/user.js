d7daa59469497b010610d7029b8f84fb6f61efdd
damnpav <32949863+damnpav@users.noreply.github.com>
2018-10-20 16:02:14 +0200

    Basic users HTTP requests done

--- Backend/code/controllers/user.js
+++ Backend/code/controllers/user.js
@@ -5,16 +5,28 @@ const userModel = require('./../schemas/user');
 module.exports.addUser = function(req, res){
     console.log('POST request');
     userModel.create(req.body)
-        .then(function(user){
-            res.send(user);
-        })
+    .then(function(user){
+        res.send(user);
+    })
 };
 
 //Requesting all users
 module.exports.getUsers = function(req, res){
     console.log('GET request');
-    userModel.find()
-        .then(function(users){
-            res.send(users)
+    userModel.find(req.params)
+    .lean()
+    .then(function(users){
+        console.log(users)
+        res.send(users)
+    })
+}
+
+module.exports.getUserById = function(req, res){
+    console.log('GETONE request');
+    userModel.findById(req.params.id)
+    .lean()
+    .then(function(user){
+        console.log(user)
+        res.send(user);
     })
 }
